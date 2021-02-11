121533134e5ed6fce850f044530b521059158426
Oscar Pallazhco <oepallaz@gmail.com>
2021-02-11 18:40:33 -0500

    sockets del lado del servidor

--- 01_band_names_app_server/index.js
+++ 01_band_names_app_server/index.js
@@ -6,10 +6,21 @@ const port = process.env.PORT;
 const app = express();
 
 const publicPath = path.resolve(__dirname, 'public');
-
 app.use(express.static(publicPath));
 
-app.listen(port, (error) => {
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+io.on('connection', client => {
+    let clientId = client.id;
+    console.log("Cliente",clientId," conectado");
+    client.on('disconnect', () => {
+        console.log("Cliente",clientId," desconectado");
+    });
+});
+
+
+
+server.listen(port, (error) => {
     if (error) {
         throw new Error(error);
     }
