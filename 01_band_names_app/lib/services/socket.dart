6532007c4115488b1dff55ec1643dce9d677db67
Oscar Pallazhco <oepallaz@gmail.com>
2021-02-12 15:32:14 -0500

    permitir usar el socket desde otro archivo

--- 01_band_names_app/lib/services/socket.dart
+++ 01_band_names_app/lib/services/socket.dart
@@ -10,26 +10,28 @@ enum ServerStatus {
 class SocketService with ChangeNotifier {
 
   ServerStatus _status = ServerStatus.Connecting;
+  IO.Socket _socket;
 
-  get getServerStatus => this._status;
+  ServerStatus get getServerStatus => this._status;
+  IO.Socket get getSocket => this._socket;
 
   SocketService() {
     this._initConfig();
   }
 
   void _initConfig() {
-    IO.Socket socket = IO.io('http://192.168.100.42:3000', {
+    this._socket = IO.io('http://192.168.100.42:3000', {
       'transports': ['websocket'],
       'autoconnect': true,
     });
 
-    socket.onConnect((_) {
+    this._socket.onConnect((_) {
       print('connect');
       this._status = ServerStatus.Online;
       notifyListeners();  //manda a actualizar a los elementos que usen este servicio
     });
 
-    socket.on('disconnect', (_) {
+    this._socket.on('disconnect', (_) {
       print('disconnect');
       this._status = ServerStatus.Offline;
       notifyListeners();  
