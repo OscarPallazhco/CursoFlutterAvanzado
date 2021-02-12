5e382ee9e5bf5c289929bccefff096468b5a144b
Oscar Pallazhco <oepallaz@gmail.com>
2021-02-12 01:56:09 -0500

    crear el servicio

---
+++ band_name_app/lib/services/socket.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+
+enum ServerStatus {
+  Online,
+  Offline,
+  Connecting,
+}
+
+class SocketService with ChangeNotifier {
+
+  ServerStatus _status = ServerStatus.Connecting;
+
+  SocketService() {
+    this._initConfig();
+  }
+
+  void _initConfig() {
+    IO.Socket socket = IO.io('http://192.168.100.42:3000', {
+      'transports': ['websocket'],
+      'autoconnect': true,
+    });
+
+    socket.onConnect((_) {
+      print('connect');
+    });
+
+    socket.on('disconnect', (_) {
+      print('disconnect');
+    });
+
+  }
+}
