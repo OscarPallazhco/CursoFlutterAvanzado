04baa2ea4d5156fdad5e39e7c3c74e5b5505bee6
Oscar Pallazhco <oepallaz@gmail.com>
2021-02-14 23:49:38 -0500

    cambiar a url desplegada

--- 01_band_names_app/lib/services/socket.dart
+++ 01_band_names_app/lib/services/socket.dart
@@ -20,7 +20,7 @@ class SocketService with ChangeNotifier {
   }
 
   void _initConfig() {
-    this._socket = IO.io('http://192.168.100.42:3000', {
+    this._socket = IO.io('https://bandnamesserver.herokuapp.com/', {
       'transports': ['websocket'],
       'autoconnect': true,
     });
