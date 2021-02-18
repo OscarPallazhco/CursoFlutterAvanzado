68cd6ef14558de5bbe6412d50b4d9e97053d605a
Oscar Pallazhco <oepallaz@gmail.com>
2021-02-18 02:18:09 -0500

    caja de texto

--- 02_chat_app/lib/pages/chat_page.dart
+++ 02_chat_app/lib/pages/chat_page.dart
@@ -1,6 +1,18 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ChatPage extends StatelessWidget {
+class ChatPage extends StatefulWidget {
+  @override
+  _ChatPageState createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  TextEditingController _chatBoxCtrler = new TextEditingController();
+  FocusNode _focusNode = new FocusNode();
+  bool _isWriting = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,14 +60,77 @@ class ChatPage extends StatelessWidget {
             ),
             Divider(),
             Container(
-              color: Colors.grey[300],
+              color: Colors.white,
               height: 50,
               width: double.infinity,
-              child: Center(child: Text('Escribir....')),
+              child: _inputChatBox(),
             )
           ],
         ),
       ),
     );
+  }
+
+  Widget _inputChatBox() {
+    return SafeArea(
+        child: Container(
+      margin: EdgeInsets.symmetric(horizontal: 8),
+      child: Row(
+        children: [
+          Flexible(
+              child: Container(
+            margin: EdgeInsetsDirectional.only(start: 2),
+            child: TextField(
+              controller: _chatBoxCtrler,
+              onSubmitted:
+                  _handleSubmit, //TODO sigue enviando a pesar de estar vacia la caja de texto
+              onChanged: (String texto) {
+                setState(() {
+                  this._isWriting = this._chatBoxCtrler.text.trim().length > 0;
+                });
+              },
+              decoration: InputDecoration.collapsed(
+                hintText: 'Enviar mensaje...',
+              ),
+              focusNode: _focusNode,
+            ),
+          )),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 4),
+            child: !Platform.isAndroid
+                ? Container(
+                    margin: EdgeInsets.symmetric(horizontal: 4),
+                    child: IconTheme(
+                      data: IconThemeData(color: Colors.blue[400]),
+                      child: IconButton(
+                        icon: Icon(
+                          Icons.send,
+                        ),
+                        onPressed: _isWriting
+                            ? () => _handleSubmit(this._chatBoxCtrler.text)
+                            : null,
+                        highlightColor: Colors.transparent,
+                        splashColor: Colors.transparent,
+                      ),
+                    ))
+                : CupertinoButton(
+                    child: Text('Enviar'),
+                    onPressed: _isWriting
+                        ? () => _handleSubmit(this._chatBoxCtrler.text)
+                        : null,
+                  ),
+          )
+        ],
+      ),
+    ));
+  }
+
+  void _handleSubmit(String texto) {
+    print(texto);
+    _chatBoxCtrler.clear();
+    _focusNode.requestFocus();
+    setState(() {
+      _isWriting = false;
+    });
   }
 }
