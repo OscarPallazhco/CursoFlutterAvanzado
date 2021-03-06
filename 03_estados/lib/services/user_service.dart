7262c671b1992f36c00b435d69b1f257c179630e
Oscar Pallazhco <oepallaz@gmail.com>
2021-03-06 12:01:21 -0500

    notifyListeners

--- 03_estados/lib/services/user_service.dart
+++ 03_estados/lib/services/user_service.dart
@@ -1,10 +1,15 @@
-import 'package:estados/models/user.dart';
 import 'package:flutter/material.dart';
+
+import 'package:estados/models/user.dart';
 
 class UserService with ChangeNotifier{
   User _user;
 
-  User get user => this._user;
+  User get getUser => this._user;
   bool get existUser => this._user!=null;
+  set setUser(User user){
+    this._user = user;
+    notifyListeners();
+  }
   
 }
