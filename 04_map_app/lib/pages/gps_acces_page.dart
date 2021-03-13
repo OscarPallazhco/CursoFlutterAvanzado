6d53f66b537749294ba1549167bee0d9c553726d
Oscar Pallazhco <oepallaz@gmail.com>
2021-03-13 12:12:31 -0500

    solicitar permisos de location

--- 04_map_app/lib/pages/gps_acces_page.dart
+++ 04_map_app/lib/pages/gps_acces_page.dart
@@ -1,12 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-class GpsAccesPage extends StatelessWidget {
+class GpsAccesPage extends StatefulWidget {
+  @override
+  _GpsAccesPageState createState() => _GpsAccesPageState();
+}
+
+class _GpsAccesPageState extends State<GpsAccesPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('GpsAccesPage'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Se requiere permisos para su uso',
+              style: TextStyle(color: Colors.black),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 10),
+              child: MaterialButton(
+                color: Colors.black,
+                shape: StadiumBorder(),
+                splashColor: Colors.transparent,
+                child: Text('Solicitar acceso', style: TextStyle(color: Colors.white),),
+                elevation: 0,
+                onPressed: () async {
+                  var status = await Permission.location.request();
+                  accesGps(status);
+                },
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
+
+  void accesGps(PermissionStatus status) {
+    switch (status) {
+      case PermissionStatus.granted:
+        print("otorgado");
+        Navigator.pushReplacementNamed(context, 'mappage');
+        break;
+      case PermissionStatus.denied:
+      case PermissionStatus.restricted:
+      case PermissionStatus.permanentlyDenied:
+        openAppSettings();
+    }
+  }
+
 }
+
+
--- 04_map_app/lib/pages/loading_page.dart
+++ 04_map_app/lib/pages/loading_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:map_app/pages/gps_acces_page.dart';
 
-import 'package:map_app/pages/map_page.dart';
 import 'package:map_app/helpers/helpers.dart';
 
 class LoadingPage extends StatelessWidget {
@@ -20,7 +20,7 @@ class LoadingPage extends StatelessWidget {
 
   Future _checkPermissions(BuildContext context) async {
     await Future.delayed(Duration(milliseconds: 2000));
-    Navigator.pushReplacement(context, navigateFadeInToPage(context, MapPage() ));
+    Navigator.pushReplacement(context, navigateFadeInToPage(context, GpsAccesPage() ));
   }
 
 }
