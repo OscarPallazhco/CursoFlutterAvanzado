ebc02d624899af77ce63f5e28e0b36ad20024aeb
Oscar Pallazhco <oepallaz@gmail.com>
2021-03-14 11:59:00 -0500

    Geolocator().isLocationServiceEnabled() para saber si está activado el gps(diferente de concedido el permiso de location)

--- 04_map_app/lib/pages/loading_page.dart
+++ 04_map_app/lib/pages/loading_page.dart
@@ -1,26 +1,72 @@
 import 'package:flutter/material.dart';
-import 'package:map_app/pages/gps_acces_page.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:map_app/pages/map_page.dart';
+import 'package:permission_handler/permission_handler.dart';
 
+import 'package:map_app/pages/gps_acces_page.dart';
 import 'package:map_app/helpers/helpers.dart';
 
-class LoadingPage extends StatelessWidget {
+class LoadingPage extends StatefulWidget {
+  @override
+  _LoadingPageState createState() => _LoadingPageState();
+}
+
+class _LoadingPageState extends State<LoadingPage> with WidgetsBindingObserver{
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addObserver(this);  //para que esté pendiente del estado de la pagina actual
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) async{
+    if (state == AppLifecycleState.resumed) {
+      print('resumed');
+        setState(() {
+        _checkGpsAndLocation(context);        
+        });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: FutureBuilder(
-        future: _checkPermissions(context),
+        future: _checkGpsAndLocation(context),
         builder: (BuildContext context, AsyncSnapshot snapshot) {
-          return Center(
-        child: CircularProgressIndicator(),
-      );
+          if (snapshot.hasData) {
+            return Center(
+              child: Text(snapshot.data),
+            );
+          }else{
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
         },
       ),
     );
   }
 
-  Future _checkPermissions(BuildContext context) async {
-    await Future.delayed(Duration(milliseconds: 2000));
-    Navigator.pushReplacement(context, navigateFadeInToPage(context, GpsAccesPage() ));
-  }
+  Future _checkGpsAndLocation(BuildContext context) async {
+    final gpsPermit = await Permission.location.isGranted;
+    final geoPermit = await Geolocator().isLocationServiceEnabled();  //versiones +6 cambia
+    // await Future.delayed(Duration(seconds: 3));
 
+    if (gpsPermit && geoPermit) {
+      Navigator.pushReplacement(context, navigateFadeInToPage(context, MapPage() ));
+      return 'permisos concedidos';
+    }else if (!gpsPermit) {
+      return 'permiso gps no concedido';
+    }else if (!geoPermit) {
+      return 'geolocator no activado';
+    }
+  }
 }
