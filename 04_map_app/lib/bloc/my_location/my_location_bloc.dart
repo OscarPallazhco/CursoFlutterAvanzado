e77d0f0f1aeec2f807869b1ba5632081809e40a2
Oscar Pallazhco <oepallaz@gmail.com>
2021-03-14 18:03:17 -0500

    Geolocator.getPositionStream para estar pendientes de la ubucación del dispositivo

--- 04_map_app/lib/bloc/my_location/my_location_bloc.dart
+++ 04_map_app/lib/bloc/my_location/my_location_bloc.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:meta/meta.dart';
 
@@ -8,10 +9,27 @@ part 'my_location_event.dart';
 part 'my_location_state.dart';
 
 class MyLocationBloc extends Bloc<MyLocationEvent, MyLocationState> {
+
+  StreamSubscription<Position> _positionSubscription;
+
   MyLocationBloc() : super(MyLocationState());
 
   @override
   Stream<MyLocationState> mapEventToState(MyLocationEvent event) async* {
     // TODO: implement mapEventToState
   }
+
+  void startTracking(){
+    _positionSubscription = Geolocator.getPositionStream(
+      desiredAccuracy: LocationAccuracy.high,
+      distanceFilter: 10
+    ).listen((Position position) { 
+      print(position);
+    });
+  }
+
+  void stopTracking() {
+    _positionSubscription?.cancel();  
+  }
+
 }
--- 04_map_app/lib/pages/map_page.dart
+++ 04_map_app/lib/pages/map_page.dart
@@ -1,6 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:map_app/bloc/my_location/my_location_bloc.dart';
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
+  @override
+  _MapPageState createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+
+  @override
+  void initState() {
+    BlocProvider.of<MyLocationBloc>(context).startTracking();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    BlocProvider.of<MyLocationBloc>(context).stopTracking();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
