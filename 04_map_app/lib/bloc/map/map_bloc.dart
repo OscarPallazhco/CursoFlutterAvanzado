5aabce852be2975d53a2d37c1a502447b30a267f
Oscar Pallazhco <oepallaz@gmail.com>
2021-03-16 11:51:18 -0500

    refactor

--- 04_map_app/lib/bloc/map/map_bloc.dart
+++ 04_map_app/lib/bloc/map/map_bloc.dart
@@ -38,23 +38,33 @@ class MapBloc extends Bloc<MapEvent, MapState> {
     if (event is OnMapIsReady) {
       yield state.copyWith(mapIsReady: true);
     }else if (event is OnMapChangeLocation) {
-      List<LatLng> points = [...this._myRoutePolyline.points, event.location];  // crear arreglo de ptos actuales más el nuevo pto
-      this._myRoutePolyline = this._myRoutePolyline.copyWith(pointsParam: points);  // actualizar el polyline actual con el nuevo arreglo de ptos
-      Map<String, Polyline> currentPolylines = state.polylines; // obtener el mapa de polylines actual del state
-      currentPolylines['myRoute'] = this._myRoutePolyline;  // actualizar en el mapa el polyline de myRoute
-      yield state.copyWith(polylines: currentPolylines);  // crear un nuevo estado con el mapa de polylines actualizado
+      yield* _onMapChangeLocation(event);
+      // yield* para regresar no el stream sino la emisión del stream
     }else if (event is OnMapShowRoute) {
-      if (!state.drawRoute) {
-        this._myRoutePolyline = this._myRoutePolyline.copyWith(colorParam: Colors.black87);
-      }else{
-        this._myRoutePolyline = this._myRoutePolyline.copyWith(colorParam: Colors.transparent);
-      }
-      Map<String, Polyline> currentPolylines = state.polylines;
-      currentPolylines['myRoute'] = this._myRoutePolyline;
-      yield state.copyWith(
-        drawRoute: !state.drawRoute,
-        polylines: currentPolylines,
-      );
+      yield* _onMapShowRoute(event);
     }
   }
+
+  Stream<MapState> _onMapChangeLocation(OnMapChangeLocation event) async*{
+    List<LatLng> points = [...this._myRoutePolyline.points, event.location];  // crear arreglo de ptos actuales más el nuevo pto
+    this._myRoutePolyline = this._myRoutePolyline.copyWith(pointsParam: points);  // actualizar el polyline actual con el nuevo arreglo de ptos
+    Map<String, Polyline> currentPolylines = state.polylines; // obtener el mapa de polylines actual del state
+    currentPolylines['myRoute'] = this._myRoutePolyline;  // actualizar en el mapa el polyline de myRoute
+    yield state.copyWith(polylines: currentPolylines);  // crear un nuevo estado con el mapa de polylines actualizado
+  }
+  
+  Stream<MapState> _onMapShowRoute(OnMapShowRoute event) async*{
+    if (!state.drawRoute) {
+      this._myRoutePolyline = this._myRoutePolyline.copyWith(colorParam: Colors.black87);
+    }else{
+      this._myRoutePolyline = this._myRoutePolyline.copyWith(colorParam: Colors.transparent);
+    }
+    Map<String, Polyline> currentPolylines = state.polylines;
+    currentPolylines['myRoute'] = this._myRoutePolyline;
+    yield state.copyWith(
+      drawRoute: !state.drawRoute,
+      polylines: currentPolylines,
+    );
+  }
+
 }
