06b589cdf7009cab7899f1ebe10e13a9feb02801
Oscar Pallazhco <oepallaz@gmail.com>
2021-03-26 19:04:43 -0500

    pagina para mostrar que se ha cancelado con éxito

--- 05_stripe_app/lib/pages/payment_completed_page.dart
+++ 05_stripe_app/lib/pages/payment_completed_page.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class PaymentCompletedPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Pago realizado'),
+        centerTitle: true,
+      ),
       body: Center(
-        child: Text('PaymentCompletedPage'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(FontAwesomeIcons.star, size: 100, color: Colors.white,),
+            SizedBox(height: 20,),
+            Text('Pago realizado existosamente!', style: TextStyle(color: Colors.white, fontSize: 22),)
+          ],
+        ),
       ),
     );
   }
