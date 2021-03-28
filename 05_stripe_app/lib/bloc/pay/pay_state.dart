88c9ec0998230c4757cf3f654e2c0cb643ce4cf0
Oscar Pallazhco <oepallaz@gmail.com>
2021-03-27 23:40:46 -0500

    .

--- 05_stripe_app/lib/bloc/pay/pay_state.dart
+++ 05_stripe_app/lib/bloc/pay/pay_state.dart
@@ -3,25 +3,25 @@ part of 'pay_bloc.dart';
 @immutable
 class PayState {
   final double amount;
-  final String coin;
+  final String currency;
   final bool creditCardIsSelected;
   final CustomCreditCard creditCard;
 
   PayState({    
     this.amount = 0.0,
-    this.coin = 'USD',
+    this.currency = 'USD',
     this.creditCardIsSelected = false,
     this.creditCard
   });
   
   PayState copyWith({
     double amount,
-    String coin,
+    String currency,
     bool creditCardIsSelected,
     CustomCreditCard creditCard
   }) => PayState(
     amount: amount ?? this.amount,
-    coin: coin ?? this.coin,
+    currency: currency ?? this.currency,
     creditCardIsSelected: creditCardIsSelected ?? this.creditCardIsSelected,
     creditCard: creditCard ?? this.creditCard,
   );
