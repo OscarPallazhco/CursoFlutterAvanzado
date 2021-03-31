15d5b75501c9e283937f9e26c69f5ab715c61495
Oscar Pallazhco <oepallaz@gmail.com>
2021-03-30 22:12:16 -0500

    botón de google sign in

--- 06_auth_app/lib/main.dart
+++ 06_auth_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
  
 void main() => runApp(MyApp());
  
@@ -11,10 +12,33 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: Text('Auth App'),
+          centerTitle: true,
         ),
-        body: Center(
-          child: Container(
-            child: Text('Auth App'),
+        body: Container(
+          padding: EdgeInsets.all(20),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                MaterialButton(
+                  splashColor: Colors.transparent,
+                  minWidth: double.infinity,
+                  height: 40,
+                  color: Colors.red,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(FontAwesomeIcons.google, color: Colors.white,),
+                      Text(' Sign in with Gooogle', style: TextStyle(color: Colors.white, fontSize: 17),)
+                    ],
+                  ),
+                  onPressed: (){
+
+                  }
+                )
+              ],
+            ),
           ),
         ),
       ),
