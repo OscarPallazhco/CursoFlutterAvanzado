231f75c4bc8f49a3ecd1f0789132640fd70c6fb3
Oscar Pallazhco <oepallaz@gmail.com>
2021-04-02 18:05:46 -0500

    terminar signin, faltaba un oauth_client(android) que había que generar en el console.cloud y descargar el nuevo google-services de firebase

--- 06_auth_app/lib/main.dart
+++ 06_auth_app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:auth_app/services/google_signin_service.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
  
@@ -7,47 +8,52 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Material App',
       debugShowCheckedModeBanner: false,
-      title: 'Auth App',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Auth App'),
-          centerTitle: true,
+          title: Text('AuthApp - Google - Apple'),
           actions: [
             IconButton(
-              icon: Icon(Icons.exit_to_app),
-              color: Colors.white,
-              onPressed: (){},
+              icon: Icon( FontAwesomeIcons.doorOpen ), 
+              onPressed: () {
+                GoogleSignInService.signOut();
+              }
             )
           ],
         ),
         body: Container(
-          padding: EdgeInsets.all(20),
+          padding: EdgeInsets.all(10),
           child: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
+
                 MaterialButton(
                   splashColor: Colors.transparent,
                   minWidth: double.infinity,
                   height: 40,
                   color: Colors.red,
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+                  shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(8)),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Icon(FontAwesomeIcons.google, color: Colors.white,),
-                      Text(' Sign in with Gooogle', style: TextStyle(color: Colors.white, fontSize: 17),)
+                      Icon( FontAwesomeIcons.google, color: Colors.white ),
+                      Text('  Sign in with Google', style: TextStyle( color: Colors.white, fontSize: 17 ),)
                     ],
                   ),
-                  onPressed: (){
+                  onPressed: () {
+                    
+                    GoogleSignInService.signInWithGoogle();
 
                   }
                 )
+
+
               ],
             ),
           ),
-        ),
+        )
       ),
     );
   }
